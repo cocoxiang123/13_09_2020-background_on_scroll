55031f6588899e26261b627ec76818a64aca1222
cocoxiang123 <cocoxiang123@users.noreply.github.com>
2020-09-13 14:01:40 +1000

    update section2 js

--- script.js
+++ script.js
@@ -8,13 +8,19 @@ $(window).scroll(function () {
   var section3_position = $(".section3").offset().top;
   var window_height = $(window).height();
 
-  if (scroll_position + window_height - section2_position > 0) {
+  if (
+    scroll_position + window_height - section2_position > 0 &&
+    scroll_position < section2_position + window_height
+  ) {
     $(".section2").css({
       "background-position-x":
         section2_position - scroll_position - window_height + "px",
     });
   }
-  if (scroll_position + window_height - section3_position > 0) {
+  if (
+    scroll_position + window_height - section3_position > 0 &&
+    scroll_position < section3_position + window_height
+  ) {
     $(".section3").css({
       "background-position-x":
         section3_position - scroll_position - window_height + "px",
